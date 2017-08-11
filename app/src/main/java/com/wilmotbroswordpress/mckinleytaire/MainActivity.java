67430c6bb97aaab4c60451ba687cd73dd62ecf28
Connor Wilmot <connorwilmot@comcast.net>
2017-08-11 16:48:57 -0400

    added click behavior to main activity buttons

--- app/src/main/java/com/wilmotbroswordpress/mckinleytaire/MainActivity.java
+++ app/src/main/java/com/wilmotbroswordpress/mckinleytaire/MainActivity.java
@@ -2,6 +2,7 @@ package com.wilmotbroswordpress.mckinleytaire;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,4 +11,10 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+    /**
+     * this method changes the activity when a button is clicked
+     * @param view
+     */
+    public void changeScreen(View view){}
 }
